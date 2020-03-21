bbb044f19fee629ff4a156dd9e0e98d52ad121ec
VietNT <viet_nguyen@t-rnd.me>
2020-03-21 22:18:54 +0900

    add search box and move to _sass folder

--- Gemfile
+++ Gemfile
@@ -1,9 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'jekyll'
+gem 'wdm'
 
 group :jekyll_plugins do
   gem "jekyll-sitemap"
   gem "jekyll-paginate"
   gem "jemoji"
+  gem 'jekyll-sass-converter'
 end
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,31 @@
-// Next and previous page by arrow key
-$('html').on('keyup', function(e) {
-    if (e.keyCode == 39) {
-        // next page
-        if ($('.older-posts').length > 0) {
-            location.href = $('.older-posts').attr('href')
-        }
+$(document).ready(function(){
+    var sjs = SimpleJekyllSearch({
+        searchInput: document.getElementById('search-input'),
+        resultsContainer: document.getElementById('results-container'),
+        json: '/search.json'
+      })
 
-    } else if (e.keyCode == 37) {
-        // previous page
-        if ($('.newer-posts').length > 0) {
-            location.href = $('.newer-posts').attr('href')
-        }
-    }
-    
+    console.log(sjs)
+  
+    // Next and previous page by arrow key
+    $('html').on('keyup', function(e) {
+        if (e.keyCode == 39) {
+            // next page
+            if ($('.older-posts').length > 0) {
+                location.href = $('.older-posts').attr('href')
+            }
+        } else if (e.keyCode == 37) {
+            // previous page
+            if ($('.newer-posts').length > 0) {
+                location.href = $('.newer-posts').attr('href')
+            }
+        }  
+    })
+     
+    var labelID;
+
+    $('label').click(function() {
+        labelID = $(this).attr('for');
+        $('#' + labelID).toggleClass('active');
+    });
 })
